ae46024cbdb5a3a9c5fe9594f4738a0328af6efb
Abhineet <abhineet_shrivastava@outlook.com>
2025-06-27 23:42:05 +0400

    Main Commit

--- terraform/aws/build.sh
+++ terraform/aws/build.sh
@@ -6,8 +6,13 @@ FUNCTIONS=("sentimentAnalyzer" "fetchSummary" "sendNotification")
 
 for func in "${FUNCTIONS[@]}"; do
   cd ../../functions/$func ##
- # npm install --production 
+  npm install #--production 
+  npm run build
+  cd dist
+  echo "Zipping $func..." 
+  
   zip -r ../../terraform/aws/$func.zip . -x "*.test.js"
   aws s3 cp ../../terraform/aws/$func.zip s3://$BUCKET_NAME/$func.zip
+  echo "Done building $func"
   cd - >/dev/null 
 done ## End
