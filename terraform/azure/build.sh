3020df543ccefdb8323001455c8bf50f4c091924
Abhineet <abhineet_shrivastava@outlook.com>
2025-06-28 19:17:21 +0400

    Main Commit

--- terraform/azure/build.sh
+++ terraform/azure/build.sh
@@ -3,12 +3,25 @@ set -e
 
 FUNCTIONS=("sentimentAnalyzer" "fetchSummary" "sendNotification")
 
+ROOT_DIR=$(pwd)/../../..   # Assuming script is at root/terraform/azure/
+ZIP_OUTPUT_DIR="$ROOT_DIR/terraform/azure"
+
 for func in "${FUNCTIONS[@]}"; do
-  cd ../../functions/$func ##
-  npm install #--production 
+  FUNC_DIR="$ROOT_DIR/functions/$func"
+  echo "Building $func from $FUNC_DIR"
+  cd "$FUNC_DIR"
+
+  npm install --silent
   npm run build
-  echo "Zipping $func..."
-  zip -r ../$func.zip dist node_modules host.json package.json .funcignore
+
+  ZIP_PATH="$ZIP_OUTPUT_DIR/$func.zip"
+  echo "Zipping $func to $ZIP_PATH..."
+  zip -r "$ZIP_PATH" dist node_modules host.json package.json .funcignore
   echo "Done building $func"
-  cd - >/dev/null 
-done ## End
+
+  cd - >/dev/null
+done
+
+# Debug: list the zip files
+echo "Contents of $ZIP_OUTPUT_DIR:"
+ls -lh "$ZIP_OUTPUT_DIR"
