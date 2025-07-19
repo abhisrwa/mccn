a77c5a500feea3069ae3c320a430c94f56024a4c
Abhineet <abhineet_shrivastava@outlook.com>
2025-07-19 17:04:47 +0400

    Main Commit

--- terraform/azure/functions.tf
+++ terraform/azure/functions.tf
@@ -99,7 +99,7 @@ resource "azurerm_windows_function_app" "fetchSummary" {
     DB_KEY          = azurerm_cosmosdb_account.cosmos.primary_key
     DB_SUMMCONTAINERID    = azurerm_cosmosdb_sql_container.sent_analysis.name
     AZQUEUE_NAME                   = azurerm_storage_queue.notification.name
-    AZQUEUE_URL                    = "https://${azurerm_storage_account.queue.name}.queue.core.windows.net/${azurerm_storage_queue.notification.name}"
+    AZQUEUE_URL                    = "https://${azurerm_storage_account.func_storage.name}.queue.core.windows.net/${azurerm_storage_queue.notification.name}"
     PLATFORM                       = "azure"
     KEY_VAULT_URL                  = azurerm_key_vault.kv.vault_uri
     CLIENT_ID                      = var.client_id
@@ -140,7 +140,7 @@ resource "azurerm_windows_function_app" "sendNotification" {
     TO_EMAIL                       = var.to_email_address
     SENDGRID_API_KEY               = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault.kv.vault_uri}/secrets/${var.azure_sendgrid_secret_name}/)"
     AZQUEUE_NAME                   = azurerm_storage_queue.notification.name
-    AZQUEUE_URL                    = "https://${azurerm_storage_account.queue.name}.queue.core.windows.net/${azurerm_storage_queue.notification.name}"
+    AZQUEUE_URL                    = "https://${azurerm_storage_account.func_storage.name}.queue.core.windows.net/${azurerm_storage_queue.notification.name}"
     PLATFORM                       = "azure"
     KEY_VAULT_URL                  = azurerm_key_vault.kv.vault_uri
     CLIENT_ID                      = var.client_id
@@ -182,7 +182,7 @@ resource "azurerm_windows_function_app" "sentimentAnalyzer" {
     SCM_DO_BUILD_DURING_DEPLOYMENT = "true"
     AzureWebJobsStorage   = azurerm_storage_account.func_storage.primary_connection_string    
 
-    AZQUEUE_URL     = "https://${azurerm_storage_account.queue.name}.queue.core.windows.net/${azurerm_storage_queue.notification.name}"
+    AZQUEUE_URL     = "https://${azurerm_storage_account.func_storage.name}.queue.core.windows.net/${azurerm_storage_queue.notification.name}"
     DB_ENDPOINT     = azurerm_cosmosdb_account.cosmos.endpoint
     DB_ID           = azurerm_cosmosdb_sql_database.database.name
     DB_KEY          = azurerm_cosmosdb_account.cosmos.primary_key
